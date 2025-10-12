ddcbb7ccfff588baef78ecaa40bdd6ce020ee6f9
ConnorC432 <connor.chetham@gmail.com>
2025-10-12 16:40:01 +0100

    metadata

--- wayne-dezza/src/app/page.js
+++ wayne-dezza/src/app/page.js
@@ -1,21 +1,35 @@
-"use client";
-
 import Layout from "../components/layout";
 import Gallery from "../components/gallery";
 import Head from "next/head";
 
+export const metadata = {
+  title: "Wayne Dezza",
+  description: "I Can't Read!",
+  openGraph: {
+    title: "Wayne Dezza",
+    description: "I Can't Read!",
+    url: "https://waynederryhasnoteeth.co.uk/",
+    type: "website",
+    images: [
+      {
+        url: "https://waynederryhasnoteeth.co.uk/images/waynedezza.jpg",
+        width: 956,
+        height: 1920,
+        alt: "Wayne Dezza",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Wayne Dezza",
+    description: "I Can't Read!",
+    images: ["https://waynederryhasnoteeth.co.uk/images/waynedezza.jpg"],
+  },
+};
+
 export default function Home() {
 	return (
 		<Layout>
-			<Head>
-				<title>Wayne Dezza</title>
-				<meta name="description" content="I Can't Read!"/>
-				<meta property="og:title" content="Wayne Dezza"/>
-				<meta property="og:description" content="I Can't Read!"/>
-				<meta property="og:image" content="https://waynederryhasnoteeth.co.uk/images/waynedezza.jpg"/>
-				<meta property="og:type" content="website"/>
-				<meta property="og:url" content="https://waynederryhasnoteeth.co.uk/"/>
-			</Head>
 			<div className="rounded-2xl p-6 bg-pink-300/30 backdrop-blur-sm shadow-inner">
 				<Gallery/>
 			</div>
